b6ee76c0d1dd19576bad20c2f5a58682d8797023
mfproduction <ferenc.matej@gmail.com>
2018-02-28 10:42:25 +0100

    added ppa clean, zsh aliases and plugins

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,11 @@
 alias reload!='. ~/.zshrc'
 alias zshc="vim ~/.zshrc"
 alias cls='clear' # Good 'ol Clear Screen command
+
+mkc () {
+  mkdir -p "$@" && cd "$@"
+}
+
+alias mcd="mkc"
+alias cmkdir="mkc"
+alias mkdirc="mkc"
