71ebe09df00da3b93e531f8f7dd346e4a317e1f6
Matej Ferenc <ferenc.matej@gmail.com>
2019-04-17 13:44:17 +0200

    ruby rails secret

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -2,3 +2,5 @@ alias sc='script/console'
 alias sg='script/generate'
 alias sd='script/destroy'
 alias sidekiq_rerun="bundle exec rerun --background --dir app,db,lib --pattern '{**/*.rb}' -- bundle exec sidekiq --verbose"
+
+alias generate_rails_secret='openssl rand -hex 64'
